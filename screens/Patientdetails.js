5d2d74d6034ded08b3c1f71fe511197efb5cc4b7
Tahir Mehmood <tahir.mehmood9425@gmail.com>
2023-03-25 10:58:01 +0500

    did some more designing

--- screens/Patientdetails.js
+++ screens/Patientdetails.js
@@ -1,4 +1,11 @@
-import {View, Text, Image, ScrollView, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 import React, {useState, useEffect} from 'react';
 
 const Patientdetails = ({route, navigation}) => {
@@ -16,48 +23,68 @@ const Patientdetails = ({route, navigation}) => {
 
   return (
     <View style={{flex: 1}}>
-      <View>
-        <Text sytle={{fontSize: 20, color: 'black'}}>Patient Details</Text>
-        <Text>Patient Name : {route.params.paramkey.p.full_name}</Text>
-        <Text>Date Of Birth : {route.params.paramkey.p.dob}</Text>
-        <Text>Gender : {route.params.paramkey.p.gender}</Text>
+      <View style={{width: '80%', marginLeft: 20}}>
+        <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
+          Patient Details:
+        </Text>
+        <Text style={{color: 'red'}}>Patient Name</Text>
+        <Text style={{marginLeft: 20, color: 'black', fontWeight: '600'}}>
+          {route.params.paramkey.p.full_name}
+        </Text>
+        <Text style={{color: 'red'}}>Date Of Birth</Text>
+        <Text style={{marginLeft: 20, color: 'black', fontWeight: '600'}}>
+          {route.params.paramkey.p.dob}
+        </Text>
+        <Text style={{color: 'red'}}>Gender</Text>
+        <Text style={{marginLeft: 20, color: 'black', fontWeight: '600'}}>
+          {route.params.paramkey.p.gender}
+        </Text>
       </View>
-      <View>
-        <Text style={{fontSize: 20, color: 'red', fontWeight: 'bold'}}>
+      <View style={{width: '80%', marginLeft: 20}}>
+        <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
           Vitals
         </Text>
-        <Text style={{color: 'green', fontSize: 20, fontWeight: 'bold'}}>
-          Blood Pressure : {route.params.paramkey.vv.blood_pressure}
-        </Text>
-        <Text style={{color: 'green', fontSize: 20, fontWeight: 'bold'}}>
-          {' '}
-          Patient Name : {route.params.paramkey.vv.sugar}
-        </Text>
-        <Text style={{color: 'green', fontSize: 20, fontWeight: 'bold'}}>
-          Patient Name : {route.params.paramkey.vv.temperature}
-        </Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{color: 'red'}}>Blood Pressure</Text>
+          <Text style={{marginLeft: 38, textDecorationLine: 'underline'}}>
+            {route.params.paramkey.vv.blood_pressure}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{color: 'red'}}> Sugar</Text>
+          <Text style={{marginLeft: 90, textDecorationLine: 'underline'}}>
+            {route.params.paramkey.vv.sugar}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{color: 'red'}}>Temperature</Text>
+          <Text style={{marginLeft: 50, textDecorationLine: 'underline'}}>
+            {route.params.paramkey.vv.temperature} F
+          </Text>
+        </View>
       </View>
-      <View style={{borderWidth: 2, borderColor: 'black', marginTop: 10}}>
-        <Text style={{fontSize: 20, color: 'red', fontWeight: 'bold'}}>
+      <View style={{flexDirection:'row'}}>
+      <View
+        style={{
+          marginTop: 10,
+          width: '40%',
+          marginLeft: 20,
+        }}>
+        <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
           Symptoms
         </Text>
-        <Text style={{fontWeight: 'bold', fontSize: 20}}>
+        <Text style={{marginLeft: 20, color: 'red', fontWeight: '600'}}>
           {route.params.paramkey.vv.symptoms}
         </Text>
       </View>
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginTop: 2
-        }}>
-        <Text style={{fontSize: 20}}>This is an image</Text>
-        <Image
-          source={{
-            uri: `http://${global.MyVar}/fyp/Content/Uploads/${imgname}`,
-          }}
-          style={{width: 200, height: 200, resizeMode: 'contain'}}
-        />
+      <View style={{width:"40%",marginTop:30,marginLeft:30}}>
+      <Image
+        source={{
+          uri: `http://${global.MyVar}/fyp/Content/Uploads/${imgname}`,
+        }}
+        style={{width: 100, height: 100, resizeMode: 'contain'}}
+      />
+      </View>
       </View>
     </View>
   );
